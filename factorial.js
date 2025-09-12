ba159db16cb5b2ae1b9ecf1f31e802bcbdeed116
charlie1234456 <charlieomogtal1@gmail.com>
2025-09-12 21:12:51 +0800

    Initial commit

---
+++ factorial.js
@@ -0,0 +1,33 @@
+const readline.require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+console.log ("--------------------------------");
+console.log ("factorial application");
+console.log ("1. say hello");
+console.log ("2. factorial");
+console.log ("3. exit application");
+console.log ("--------------------------------");
+
+rl.question("inter your choice (  1-3):", choice =>{)
+       console.log(choice)
+
+   if (choice === 1){
+         console.log("hello ");
+   }else if (choice === "2"){
+       console.log("factorial");
+       
+         rl.question ("please enter a number for factorial", numStr =>{
+            let num = parseInt(numStr);
+
+            if ()
+         })isNaN(num){
+
+        }
+
+    }
+}
